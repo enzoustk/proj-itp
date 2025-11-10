c701abd9f6fe59b54ccfbbf00298aa2401359d9b
Heitor Sena <marizheitor@gmail.com>
2025-11-10 16:48:28 -0300

    Etapa 0

---
+++ proj-itp/descricao/sequencia.h
@@ -0,0 +1,53 @@
+#include <iostream>
+
+template <typename T>
+class Sequencia {
+    int capacidade;
+    int tamanho;
+    T *dados;
+
+    void aumentaCapacidade() {
+        capacidade *= 2;
+        T *novo = new T[capacidade];
+        for(int i = 0; i < tamanho; i++) {
+            novo[i] = dados[i];
+        }
+        delete[] dados;
+        dados = novo;
+    }
+
+public:
+    Sequencia(int cap = 2): capacidade(cap), tamanho(0)  {
+        dados = new T[capacidade];
+    }
+
+    ~Sequencia() {
+        delete[] dados;
+    }
+
+    int obterTamanho() {
+        return tamanho;
+    }
+
+    void adicionar(T valor) {
+        if (tamanho >= capacidade) {
+            aumentaCapacidade();
+        }
+        dados[tamanho] = valor;
+        tamanho++;
+    }
+
+    T obterElememto(int indice) {
+        if (indice < 0 || indice >= tamanho) {
+            std::cerr << "Erro: indice inválido";
+        }
+        return dados[indice];
+    }
+
+    void removerUltimo() {
+        if (tamanho <= 0) {
+            std::cerr << "Erro: remoção de sequencia vazia";
+        }
+        tamanho--;
+    }
+};
