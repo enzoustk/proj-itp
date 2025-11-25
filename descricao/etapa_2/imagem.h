f4fc24464e3277bb6e570f217545f65e054c458d
Heitor Sena <marizheitor@gmail.com>
2025-11-24 22:34:37 -0300

    Etapa 2: Finalizada.

--- descricao/etapa_2/imagem.h
+++ descricao/etapa_2/imagem.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 
@@ -50,7 +51,11 @@ class Imagem{
 
     Pixel& operator()(int x,int y){
         return imagem[y][x];
-    }/////////////////////////////////////
+    }
+
+    const Pixel& operator()(int x, int y) const {
+    return imagem[y][x]; 
+    }
 
     ~Imagem(){
         desalocar();
@@ -81,6 +86,36 @@ class Imagem{
                 arquivo >> imagem[i][j].r;
                 arquivo >> imagem[i][j].g; 
                 arquivo >> imagem[i][j].b; 
+            }
+        }
+        return true;
+    }
+
+    bool salvarPPM(const string& nomeArquivo){
+        ofstream arquivo(nomeArquivo);
+        if(!arquivo.is_open()){
+            return false;
+        }
+        arquivo << "P3" << endl;
+        arquivo << coluna << " " << linha << endl;
+        int maiorValor = 0;
+        for (int i = 0;i < linha;i++){
+            for(int j = 0;j < coluna;j++){
+                if (imagem[i][j].r > maiorValor){
+                    maiorValor = imagem[i][j].r;
+                }
+                if (imagem[i][j].g > maiorValor){
+                    maiorValor = imagem[i][j].g;
+                }
+                if (imagem[i][j].b > maiorValor){
+                    maiorValor = imagem[i][j].b;
+                }
+            }
+        }
+        arquivo << maiorValor << endl;
+        for (int i = 0;i < linha;i++){
+            for(int j = 0;j < coluna;j++){
+                arquivo << imagem[i][j].r << " " << imagem[i][j].g << " " << imagem[i][j].b << endl;
             }
         }
         return true;
